1db5ca1e166db69957acd8826f74fd491af5b2c9
Thiago Alonso <thralonso@gmail.com>
2023-07-03 00:53:40 +0100

    feat: adds openai summarizer

---
+++ src/components/Demo.jsx
@@ -0,0 +1,115 @@
+import { useState, useEffect } from "react";
+import { useLazyGetSummaryQuery } from "../services/article";
+import { copy, linkIcon, loader, tick } from "../assets";
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  useEffect(() => {
+    const localStorageArticles = JSON.parse(
+      localStorage.getItem("allArticles")
+    );
+    if (localStorageArticles) {
+      setAllArticles(localStorageArticles);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedArticles = [...allArticles, newArticle];
+      setArticle(newArticle);
+      setAllArticles(updatedArticles);
+      localStorage.setItem("allArticles", JSON.stringify(updatedArticles));
+    }
+  };
+
+  const handleCopy = (copiedUrl) => {
+    setCopied(copiedUrl);
+    navigator.clipboard.writeText(copiedUrl);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-300"
+          >
+            🚀
+          </button>
+        </form>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((article, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(article)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(article.url)}>
+                <img
+                  src={copied === article.url ? tick : copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-slate-400 font-medium text-sm truncate">
+                {article.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="text-red-500 text-center font-bold font-inter">
+            <span>{error?.data?.error}</span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600">
+                Article<span className="orange_gradient"> Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
