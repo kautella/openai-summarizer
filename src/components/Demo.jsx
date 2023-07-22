2db8a7d330e0856e595f3774803fed8f9d053cab
kautella <thralonso@gmail.com>
2023-07-22 21:41:14 +0100

    debug: adds info on env variables

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -20,6 +20,7 @@ const Demo = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(import.meta.env.VITE_RAPID_API_ARTICLE_KEY);
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
